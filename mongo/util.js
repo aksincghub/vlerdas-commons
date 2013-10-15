7d9a77c2286a57e7c78b913f3c2362427c90f467
Julian Jewel <julian@julianjewel.com>
2013-10-15 09:17:42 -0700

    Fixed bug with docs to doc

--- mongo/util.js
+++ mongo/util.js
@@ -22,19 +22,20 @@ module.exports = util = {
 		if(_.isObject(doc)) {
 			if(_.isObject(doc._id)) {
 				doc._id = doc._id.toHexString();
-			} else if(_.isObject(doc.obj._id)) {
+			} else if(_.isObject(doc.obj) && _.isObject(doc.obj._id)) {
 				// Special case for text searches
 				doc.obj._id = doc.obj._id.toHexString();
 			} else if (_.isObject(doc.fileUpload) && _.isObject(doc.fileUpload.id)) {
 				doc.fileUpload.id = doc.fileUpload.id.toHexString();
-			} else if (_.isObject(docs.filedata) && _.isObject(docs.filedata.id)) {
-				docs.filedata.id = docs.filedata.id.toHexString();
+			} else if (_.isObject(doc.filedata) && _.isObject(doc.filedata.id)) {
+				doc.filedata.id = doc.filedata.id.toHexString();
 			}
 			
 			if(_.isObject(doc.uploadDate)) {
 				doc.uploadDate = doc.uploadDate.toISOString();
 			}
 		}
+		return doc;
 	},
 	getTimestamp: function(date) {
 		var time;
