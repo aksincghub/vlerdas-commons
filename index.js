b774c3af67f3a5a9e729c613394b05ae92694b7e
David Vazquez <david.vazquez@va.gov>
2013-10-17 15:54:58 -0500

    Added index.js that exports public child modules.

---
+++ index.js
@@ -0,0 +1,9 @@
+exports = module.exports;
+
+exports.accessControl = require('./access/accessControl');
+exports.bodyParser = require('./express/bodyParser');
+exports.multipartBodyParser = require('./express/multipartBodyParser');
+exports.mongoUtil = require('./mongo/util');
+exports.util = require('./util/util');
+exports.objTree = require('./xml/js-objTree');
+
