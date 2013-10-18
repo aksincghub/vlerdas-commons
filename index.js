49c41b4d362fe0712a61b4a04dea54357b7b0386
Julian Jewel <julian@julianjewel.com>
2013-10-17 21:23:18 -0700

    Added logging and try/catch IN INDEX
    The require like mongodb is not needed for all modules. try/catch is
    just a temp. fix

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 exports = module.exports;
+try {
+	exports.accessControl = require('./access/accessControl');
+} catch (err) {
+	console.warn('Library vcommons:accessControl Library is not loaded');
+}
 
-exports.accessControl = require('./access/accessControl');
-exports.bodyParser = require('./express/bodyParser');
-exports.multipartBodyParser = require('./express/multipartBodyParser');
-exports.mongoUtil = require('./mongo/util');
-exports.util = require('./util/util');
-exports.objTree = require('./xml/js-objTree');
-
+try {
+	exports.log = require('./log/logger');
+} catch (err) {
+	console.warn('Library vcommons:logger Library is not loaded');
+}
+try {
+	exports.util = require('./util/util');
+} catch (err) {
+	console.warn('Library vcommons:util Library is not loaded');
+}
+try {
+	exports.objTree = require('./xml/js-objTree');
+} catch (err) {
+	console.warn('Library vcommons:ObjTree is not loaded');
+}
+try {
+	exports.bodyParser = require('./express/bodyParser');
+} catch (err) {
+	console.warn('Library vcommons:bodyParser is not loaded');
+}
+try {
+	exports.multipartBodyParser = require('./express/multipartBodyParser');
+} catch (err) {
+	console.warn('Library vcommons:multipartBodyParser  is not loaded');
+}
+try {
+	exports.mongoUtil = require('./mongo/util');
+} catch (err) {
+	console.warn('Library vcommons:mongoUtil is not loaded');
+}
