2ac7ec0e98e34a64ca788928a383a7eb98fc8a11
Julian Jewel <julian@julianjewel.com>
2013-10-18 13:04:24 -0700

    Added errors

--- index.js
+++ index.js
@@ -2,36 +2,36 @@ exports = module.exports;
 try {
 	exports.accessControl = require('./access/accessControl');
 } catch (err) {
-	console.warn('Library vcommons:accessControl Library is not loaded');
+	console.warn('Library vcommons:accessControl Library is not loaded. Error:' + err);
 }
 
 try {
 	exports.log = require('./log/logger');
 } catch (err) {
-	console.warn('Library vcommons:logger Library is not loaded');
+	console.warn('Library vcommons:logger Library is not loaded. Error:' + err);
 }
 try {
 	exports.util = require('./util/util');
 } catch (err) {
-	console.warn('Library vcommons:util Library is not loaded');
+	console.warn('Library vcommons:util Library is not loaded. Error:' + err);
 }
 try {
-	exports.objTree = require('./xml/js-objTree');
+	exports.objTree = require('./xml/js-ObjTree');
 } catch (err) {
-	console.warn('Library vcommons:ObjTree is not loaded');
+	console.warn('Library vcommons:ObjTree is not loaded. Error:' + err);
 }
 try {
 	exports.bodyParser = require('./express/bodyParser');
 } catch (err) {
-	console.warn('Library vcommons:bodyParser is not loaded');
+	console.warn('Library vcommons:bodyParser is not loaded. Error:' + err);
 }
 try {
 	exports.multipartBodyParser = require('./express/multipartBodyParser');
 } catch (err) {
-	console.warn('Library vcommons:multipartBodyParser  is not loaded');
+	console.warn('Library vcommons:multipartBodyParser  is not loaded. Error:' + err);
 }
 try {
 	exports.mongoUtil = require('./mongo/util');
 } catch (err) {
-	console.warn('Library vcommons:mongoUtil is not loaded');
+	console.warn('Library vcommons:mongoUtil is not loaded. Error:' + err);
 }
